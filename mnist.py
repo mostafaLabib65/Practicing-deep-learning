511963627ac8d6bef6c4cedb0670c8985803f5b2
mostafa <mostafa.labib65@gmail.com>
2018-07-22 20:54:58 +0200

    adding visual testing after training

--- mnist.py
+++ mnist.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
-
+import time
 
 
 
@@ -17,6 +17,7 @@ batch_size = 100
 
 x = tf.placeholder('float',[None, 784])
 y = tf.placeholder('float')
+
 
 def neural_network_model(data):
     hidden_layers = [tf.Variable] * 3
@@ -37,15 +38,24 @@ def neural_network_model(data):
     return output
 
 
+def gen_image(arr):
+    two_d = (np.reshape(arr, (28, 28)) * 255).astype(np.uint8)
+    plt.imshow(two_d, interpolation='nearest')
+    return plt
+
+
 def train_neural_network(x,y):
     pridection = neural_network_model(x)
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pridection,labels=y))
     optimizor = tf.train.AdamOptimizer().minimize(cost)
+    tf_x = tf.placeholder('float', [None, 784])
+    tf_p = tf.placeholder('float')
 
-    epochs = 1
+    epochs = 6
 
     with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
+        saver = tf.train.Saver()
         for epoch in range(epochs):
             epoch_loss = 0
             for _ in range(int(mnist.train.num_examples/batch_size)):
@@ -53,15 +63,20 @@ def train_neural_network(x,y):
                 _, c = sess.run([optimizor, cost], feed_dict={x: epoch_x, y: epoch_y})
                 epoch_loss += c
             print('Epoch ', epoch, 'completed out of', epochs, 'loss ', epoch_loss)
-
         correct = tf.equal(tf.argmax(pridection, 1), tf.argmax(y,1))
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
         print('Accuracy: ', accuracy.eval({x:mnist.test.images, y: mnist.test.labels}))
 
 
+        test_x = mnist.test.images
+        p = tf.argmax(pridection, 1)
+        p = tf.cast(p, 'float')
+        p = p.eval({x: test_x})
+        while True:
+            n = np.random.randint(0, p.shape[0])
+            print('Model Prediction:', p[n])
+            gen_image(test_x[n]).show()
+
+
 
 train_neural_network(x,y)
-
-
-
-
